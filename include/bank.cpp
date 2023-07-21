e0816ee2183a8856bfcab0702fa29519728e74d3
nocturus <chryripple@gmail.com>
2023-07-21 18:17:17 +0200

    Adding `randint` and finishing bank class

---
+++ include/bank.cpp
@@ -0,0 +1,29 @@
+#include "bank.hpp"
+
+#include <cstdlib>
+#include <random>
+
+namespace bnk {
+int bank::get_capital() { return this->capital; }
+
+int bank::get_customers() { return this->customers_num; }
+
+// Get random interger
+static int randint(int n) {
+  std::random_device rd;   // obtain a random number from hardware
+  std::mt19937 gen(rd());  // seed the generator
+  std::uniform_int_distribution<> distr(0, n - 1);  // define the range
+  return distr(gen);                                // generate a random integer
+}
+
+// Add to customers
+int bank::add_to_customers(std::string name, int capital) {
+  int rand_num = randint(10000);  // generate a random integer between 0 and 99
+  this->customers.insert(
+      {rand_num, capital});  // use the random integer as a key when inserting a
+                             // new customer
+  ++this->customers_num;
+
+  return rand_num;
+}
+}  // namespace bnk
---
+++ include/bank.hpp
@@ -0,0 +1,29 @@
+#include <assert.h>
+
+#include <map>
+#include <memory>
+#include <random>
+
+#include "files.hpp"
+
+namespace bnk {
+class bank {
+ public:
+  int get_capital();
+
+  bank(int capital) {
+    this->f->write_to_file("Bank set up with capital of " +
+                           std::to_string(this->get_capital()));
+  }
+
+  int get_customers();
+
+  int add_to_customers(std::string name, int capital);
+
+ private:
+  int capital;
+  std::unique_ptr<files::file> f = std::make_unique<files::file>("bank.txt");
+  std::map<int, int> customers;
+  int customers_num;
+};
+}  // namespace bnk
