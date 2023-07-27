a11145299781932be4795e784e98e507bc9a9fca
nocturus <chryripple@gmail.com>
2023-07-27 20:14:47 +0200

    Changing names

--- impl/yaml.cpp
+++ impl/yaml.cpp
@@ -4,19 +4,19 @@ namespace bnk {
 void yaml::add_to_yaml(std::vector<std::string> arr) {
   YAML::Emitter out(this->outfile);  // Create an empty map to store the
                                      // key-value pairs from the input array
-  std::map<std::string, std::string> myMap;
+  std::map<std::string, std::string> map;
 
   // Iterate over the elements of the input array in pairs
   for (int i = 0; i < arr.size(); i += 2) {
     // Use the first element of each pair as the key and the second element as
     // the value in the map
-    myMap[arr[i]] = arr[i + 1];
+    map[arr[i]] = arr[i + 1];
   }
 
   // Output the map in YAML format to the stream that the YAML emitter is
   // calling
   out << YAML::BeginMap;
-  for (auto const& x : myMap) {
+  for (auto const& x : map) {
     out << YAML::Key << x.first;
     out << YAML::Value << x.second;
   }
